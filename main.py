5b80351ba3c56f2f267d76367d490bac4b5bbdfa
ultron2000 <98109711+ultron2000@users.noreply.github.com>
2022-02-17 10:48:07 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import turtle
 terry = turtle.Turtle()
 
 
-def draw_square():
+def draw_square(input_color):
     global terry
 
     terry.showturtle()
     terry.pendown()
 
-    terry.color("green")
+    terry.color(input_color)
     terry.begin_fill()
 
     terry.forward(200)
@@ -26,14 +26,15 @@ def draw_square():
     terry.penup()
 
 
-def draw_rectangle():
+def draw_rectangle(input_color):
     global terry
 
     terry.showturtle()
+    terry.penup()
     terry.goto(-100, -300)
     terry.pendown()
 
-    terry.color("yellow")
+    terry.color(input_color)
     terry.begin_fill()
 
     terry.forward(400)
@@ -49,14 +50,15 @@ def draw_rectangle():
     terry.penup()
 
 
-def draw_triangle():
+def draw_triangle(input_color):
     global terry
 
     terry.showturtle()
+    terry.penup()
     terry.goto(-300, 0)
     terry.pendown()
 
-    terry.color("red")
+    terry.color(input_color)
     terry.begin_fill()
 
     terry.forward(200)
@@ -70,14 +72,15 @@ def draw_triangle():
     terry.penup()
 
 
-def draw_circle():
+def draw_circle(input_color):
     global terry
 
     terry.showturtle()
+    terry.penup()
     terry.goto(-100, 150)
     terry.pendown()
 
-    terry.color("blue")
+    terry.color(input_color)
     terry.begin_fill()
 
     terry.circle(50)
@@ -87,10 +90,22 @@ def draw_circle():
 
 
 def main():
-    draw_square()
-    draw_rectangle()
-    draw_triangle()
-    draw_circle()
+    print("Please enter a shape, the choices are: square, rectangle, triangle, circle.")
+    input_shape = input("Please choose your desired shape: ")
+    print("Please enter a color for the shape")
+    input_color = input("Pick a color!: ")
+    if input_shape == "square":
+        draw_square(input_color)
+
+    if input_shape == "rectangle":
+        draw_rectangle(input_color)
+
+    if input_shape == "triangle":
+        draw_triangle(input_color)
+
+    if input_shape == "circle":
+        draw_circle(input_color)
+
     turtle.done()
 
 
